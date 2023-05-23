cd1aaec0e5f291d07b3c025f816cf1bf82dfbd25
Usmanasim11 <131867634+Usmanasim11@users.noreply.github.com>
2023-05-23 12:28:05 +0500

    Update docker-setup.sh

--- docker-setup.sh
+++ docker-setup.sh
@@ -5,10 +5,10 @@ sudo yum -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin d
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo systemctl status docker
-sudo usermod -aG docker username 
+sudo usermod -aG docker centos 
 sudo systemctl restart docker 
 echo "Wait for 5 Sec"
-sleep(5)
+sleep 5
 sudo chmod 666 /var/run/docker.sock 
 sudo systemctl restart docker 
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.12.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
